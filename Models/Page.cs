6f9438997e0c497dfe7ae5aaf8fedc3a12d8eb62
VoDev01 <vodev1405@gmail.com>
2023-07-25 22:03:17 +0300

    products_filter branch.

--- Models/Page.cs
+++ Models/Page.cs
@@ -7,7 +7,7 @@ namespace Silerium.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+        public int Idf { get; set; }
         public List<Product> Products { get; set; }
     }
 }
