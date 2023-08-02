b59ad0790a40fe6ac670277ea42c262a68c953ee
VoDev01 <vodev1405@gmail.com>
2023-08-02 12:28:47 +0300

    Created pages for interaction with profile.

---
+++ ViewModels/UserLoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Silerium.Models;
+
+namespace Silerium.ViewModels
+{
+    public class UserLoginViewModel
+    {
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
