1af5aed516c7c8474ae7d2f91f8d5aaa2e1538a8
VoDev01 <vodev1405@gmail.com>
2023-08-11 23:02:35 +0300

    Added files.

---
+++ DTO/ProductsFiltersDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Silerium.DTO
+{
+    public class ProductsFiltersDTO
+    {
+        public string CategoryName { get; set; }
+        public string SubcategoryName { get; set; }
+        public string SortOrder { get; set; }
+        public int CurrentPage { get; set; } = 1;
+    }
+}
---
+++ Data/Configurations/OrderStatusConverter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Silerium.Models;
+using System.Linq.Expressions;
+
+namespace Silerium.Data.Configurations
+{
+    public class OrderStatusConverter : ValueConverter<OrderStatus, string>
+    {
+        public OrderStatusConverter() : base(
+            str => str.ToString(),
+            os => (OrderStatus)Enum.Parse(typeof(OrderStatus), os))
+        {
+        }
+    }
+}
